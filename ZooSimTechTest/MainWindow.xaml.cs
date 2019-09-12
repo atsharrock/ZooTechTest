a4a383af155ae3ce13cfe0a2b91087d2d349f479
atsharrock <atsharrock@yahoo.co.uk>
2019-09-12 23:45:47 +0100

    Project name change.

---
+++ ZooSimTechTest/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ZooSimTechTest
+{
+    public partial class MainWindow : Window
+    {
+
+        private readonly Zoo _zoo;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _zoo = new Zoo();
+            DataContext = _zoo;
+        }
+
+        private void FeedButton_Click(object sender, RoutedEventArgs e)
+        {
+            Feeding.Feed(_zoo.Monkeys);
+            Feeding.Feed(_zoo.Giraffes);
+            Feeding.Feed(_zoo.Elephants);
+        }
+        
+    }
+}
